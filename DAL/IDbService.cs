874d18ef04aacedd15c47d7c75c94a2f64c65de5
Koshatina <s19575@pjwstk.edu.pl>
2020-12-07 15:03:48 +0100

    Initial commit

---
+++ DAL/IDbService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace cw3.DAL
+{
+    public interface IDbService<T>
+    {
+        public IEnumerable<T> GetEntries();
+        public T GetEntry(int id);
+        public int NextId();
+        public void AddEntry(T entryToAdd);
+        public void RemoveEntry(T entryToRemove);
+    }
+}
